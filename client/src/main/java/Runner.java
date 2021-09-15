143537c6c8aa518c8ddbdc462dd85b710fc1a1d2
alex <elbaglikov@gmail.com>
2021-09-15 18:03:42 +0300

    runner add join

--- client/src/main/java/Runner.java
+++ client/src/main/java/Runner.java
@@ -15,37 +15,39 @@ public class Runner {
     private static final Logger LOGGER = Logger.getLogger(Runner.class);
 
     public static void main(String[] args) {
-            GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext();
-            LOGGER.info("start application");
-            ResourceBundle rb = ResourceBundle.getBundle("application");
-            final int M_PARAMETER = Integer.parseInt(rb.getString("key.m"));
-            int N_PARAMETER = Integer.parseInt(rb.getString("key.n"));
-            int K_PARAMETER = Integer.parseInt(rb.getString("key.k"));
+            try(GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()) {
+                LOGGER.info("start application");
+                ResourceBundle rb = ResourceBundle.getBundle("application");
+                final int M_PARAMETER = Integer.parseInt(rb.getString("key.m"));
+                int N_PARAMETER = Integer.parseInt(rb.getString("key.n"));
+                int K_PARAMETER = Integer.parseInt(rb.getString("key.k"));
 
-            appCtx.load(
-                    "spring/spring-app.xml",
-                    "spring/spring-mvc.xml",
-                    "spring/couchbase.xml",
-                    "spring/amqp.xml");
-            appCtx.refresh();
-            UserClient userClient = appCtx.getBean(UserClient.class);
-            UserService userService = appCtx.getBean(UserService.class);
+                appCtx.load(
+                        "spring/spring-app.xml",
+                        "spring/spring-mvc.xml",
+                        "spring/couchbase.xml",
+                        "spring/amqp.xml");
+                appCtx.refresh();
+                UserClient userClient = appCtx.getBean(UserClient.class);
+                UserService userService = appCtx.getBean(UserService.class);
 
-        try {
-            GetDataFromServerRunner getDataFromServerRunner = new GetDataFromServerRunner(userClient, N_PARAMETER);
-            Thread dateFromServerGetter = new Thread(getDataFromServerRunner);
+                try {
+                    GetDataFromServerRunner getDataFromServerRunner = new GetDataFromServerRunner(userClient, N_PARAMETER);
+                    Thread dateFromServerGetter = new Thread(getDataFromServerRunner);
 
-            SendMessageRunner sendMessageRunner = new SendMessageRunner(userService, M_PARAMETER, K_PARAMETER, appCtx);
-            Thread messageSender = new Thread(sendMessageRunner);
+                    SendMessageRunner sendMessageRunner = new SendMessageRunner(userService, M_PARAMETER, K_PARAMETER, appCtx);
+                    Thread messageSender = new Thread(sendMessageRunner);
 
-            dateFromServerGetter.start();
-            messageSender.start();
-        } catch (TransformXMLException | QueryServerException e) {
-            LOGGER.error(e.getMessage());
-        }
-        finally {
-            userService.deleteAll();
-        }
+                    dateFromServerGetter.start();
+                    messageSender.start();
+                    dateFromServerGetter.join();
+                    messageSender.join();
+                } catch (TransformXMLException | QueryServerException | InterruptedException e) {
+                    LOGGER.error(e.getMessage());
+                }  finally {
+                    userService.deleteAll();
+                }
+            }
     }
 
 }
